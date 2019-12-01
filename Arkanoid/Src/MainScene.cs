be2a3190b282488b4092a654cd120f9dae6b8d20
Filip Hausman <filip.hausman@gmail.com>
2019-12-01 22:21:14 +0100

    adding dynamically loaded number of chances indicator

--- Arkanoid/Src/MainScene.cs
+++ Arkanoid/Src/MainScene.cs
@@ -1,10 +1,14 @@
 using Godot;
 using System;
+using System.Linq;
 
 public class MainScene : Node2D
 {
-    public int NumberOfLives { get; set; } = 2; 
+    public int NumberOfLives { get; set; } = 2;
     private Node2D blocks;
+    private Control ui;
+    private Control lifeContainer;
+    private PackedScene boardIcon;
     void CheckIfPlayerDestroyedAllBlocks()
     {
         var blocks_count = blocks.GetChildCount();
@@ -34,7 +38,15 @@ public class MainScene : Node2D
 
     public override void _Ready()
     {
+
+        boardIcon = GD.Load<PackedScene>("res://Resources/UI/BoardIcon.tscn");
         blocks = GetNode<Node2D>("Blocks");
         GetNode("Ball").Connect("CheckWin", this, nameof(CheckIfPlayerDestroyedAllBlocks));
+        ui = GetNode<Control>("UI");
+        lifeContainer = ui.GetNode<Control>("LifeContainer");
+        foreach(var i in Enumerable.Range(1, NumberOfLives))
+        {
+            lifeContainer.AddChild(boardIcon.Instance());
+        }
     }
 }
