8c3f59fe92d3862426245dfe5607ae26e502d8e2
Filip Hausman <filip.hausman@gmail.com>
2019-12-01 14:37:54 +0100

    connecting checking win coditions to ball signal

--- Arkanoid/Src/MainScene.cs
+++ Arkanoid/Src/MainScene.cs
@@ -3,11 +3,21 @@ using System;
 
 public class MainScene : Node2D
 {
+    void CheckIfPlayerDestroyedAllBlocks()
+    {
+        GD.Print("Checkin...");
+    }
     public void OnDeathAreaEntered(PhysicsBody2D body)
     {
-        if (body.Name == "Ball")
+        if (body is Ball ball)
         {
+            ball.ResetState();
             GD.Print("Ball entered death zone");
         }
+    }
+
+    public override void _Ready()
+    {
+        GetNode("Ball").Connect("CheckWin", this, nameof(CheckIfPlayerDestroyedAllBlocks));
     }
 }
