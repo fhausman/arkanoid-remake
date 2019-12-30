94e47c7aa1fd45c0886602f1d5050bf63ed2d00b
Filip Hausman <filip.hausman@gmail.com>
2019-12-31 00:12:20 +0100

    adding existing powerups to level manager

--- Arkanoid/Src/PowerupManager.cs
+++ Arkanoid/Src/PowerupManager.cs
@@ -3,19 +3,38 @@ using System;
 
 public class PowerupManager : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export(PropertyHint.Range, "0,1")]
+    public float PowerupSpawnProbability = 0.0f;
+    private MainScene scene;
+    private PackedScene laser;
+    private PackedScene multiball;
+    private PackedScene extraLife;
+    private PackedScene boardExtension;
+    private PackedScene slowdown;
+    private PackedScene glue;
+    //todo private PackedScene teleport;
+    private bool IsMultiballActive { get => scene.GetTree().GetNodesInGroup("BALLS").Count > 1; }
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    static private PowerupManager powerupManager;
+
+    static public void SpawnPowerup(Vector2 blockPosition)
     {
-        
+        if(powerupManager.IsMultiballActive == false)
+        {
+
+        }
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _Ready()
+    {
+        powerupManager = this;
+
+        scene = GetNode<MainScene>("/root/Main");
+        laser = GD.Load<PackedScene>("res://Resources/PowerUps/Laser.tscn");
+        multiball = GD.Load<PackedScene>("res://Resources/PowerUps/Multiball.tscn");
+        extraLife = GD.Load<PackedScene>("res://Resources/PowerUps/ExtraLife.tscn");
+        boardExtension = GD.Load<PackedScene>("res://Resources/PowerUps/BoardExtend.tscn");
+        slowdown = GD.Load<PackedScene>("res://Resources/PowerUps/Slowdown.tscn");
+        glue = GD.Load<PackedScene>("res://Resources/PowerUps/glue.tscn");
+    }
 }
