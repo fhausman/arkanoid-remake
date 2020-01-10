25821419971e8a1f55d6003a03dcec1ae262bf29
Filip Hausman <filip.hausman@gmail.com>
2020-01-10 23:45:18 +0100

    mainmenu sounds

--- Arkanoid/Src/MainMenu.cs
+++ Arkanoid/Src/MainMenu.cs
@@ -6,6 +6,8 @@ public class MainMenu : Control
     PackedScene mainScene;
     PackedScene credits;
     AudioStreamPlayer2D menuSound;
+    AudioStreamPlayer2D startSound;
+    bool startGame = false;
 
     public void OnQuitPressed()
     {
@@ -14,7 +16,9 @@ public class MainMenu : Control
 
     public void OnStartPressed()
     {
-        GetTree().ChangeSceneTo(mainScene);
+        GetNode<AudioStreamPlayer2D>("StartSound").Play();
+        startGame = true;
+        GetTree().Paused = true;
     }
 
     public void OnCreditsPressed()
@@ -32,6 +36,15 @@ public class MainMenu : Control
         mainScene = GD.Load<PackedScene>("MainScene.tscn");
         credits = GD.Load<PackedScene>("Credits.tscn");
         menuSound = GetNode<AudioStreamPlayer2D>("MenuSound");
+        startSound = GetNode<AudioStreamPlayer2D>("StartSound");
         GetNode<Button>("VBoxContainer/Start").GrabFocus();
+    }
+
+    public override void _Process(float delta)
+    {
+        if(startGame && !startSound.IsPlaying())
+        {
+            GetTree().ChangeSceneTo(mainScene);
+        }
     }
 }
